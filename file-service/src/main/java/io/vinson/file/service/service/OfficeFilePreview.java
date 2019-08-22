8689fcd5ce624bbbd9bf6d4fc025410a174a60eb
vinsonJiang <vinsonJiangx@qq.com>
2019-08-22 22:35:55 +0800

    office转pdf

--- file-service/src/main/java/io/vinson/file/service/service/OfficeFilePreview.java
+++ file-service/src/main/java/io/vinson/file/service/service/OfficeFilePreview.java
@@ -2,13 +2,20 @@ package io.vinson.file.service.service;
 
 import io.vinson.file.service.model.FileAttribute;
 import io.vinson.file.service.util.FileUtil;
+import io.vinson.file.service.util.OfficeToPdf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
 
 public class OfficeFilePreview implements FilePreview {
     private final Logger logger = LoggerFactory.getLogger(OfficeFilePreview.class);
+
+    public static final String fileDir = "D://";
+
+    @Autowired
+    private OfficeToPdf officeToPdf;
 
     public String filePreviewHandle(String url) {
         FileAttribute fileAttribute = FileUtil.getFileAttribute(url);
@@ -17,6 +24,11 @@ public class OfficeFilePreview implements FilePreview {
         String decodedUrl = fileAttribute.getDecodedUrl();
         boolean isHtml = suffix.equalsIgnoreCase("xls") || suffix.equalsIgnoreCase("xlsx");
         String pdfName = fileName.substring(0, fileName.lastIndexOf(".") + 1) + (isHtml ? "html" : "pdf");
+
+        String filePath = fileDir + fileName;
+        String outFilePath = fileDir + pdfName;
+        officeToPdf.office2pdf(filePath, outFilePath);
+
         return isHtml ? "html" : "pdf";
     }
 }
